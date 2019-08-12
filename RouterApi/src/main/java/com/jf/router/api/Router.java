a66f9c7660319d16deb5eb141da557be0b0f9c76
haozi <263667227@qq.com>
2019-08-13 00:28:56 +0800

    更新ROUTER

--- RouterApi/src/main/java/com/jf/router/api/Router.java
+++ RouterApi/src/main/java/com/jf/router/api/Router.java
@@ -1,4 +1,8 @@
-package com.jf.skinchange.router.api;
+package com.jf.router.api;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,6 +10,7 @@ import java.util.Map;
 public class Router {
 
     private Map<String,Class> routerMap = new HashMap<>();
+    private Context mContext;
 
     private Router(){}
 
@@ -17,13 +22,21 @@ public class Router {
         return SingleHolder.instance;
     }
 
+
+    private void init(Application application){
+        this.mContext = application;
+    }
+
     public void registRoute(String path,Class<?> cls){
         routerMap.put(path,cls);
     }
 
     public void jump(String path){
         if(routerMap.get(path) != null){
-
+            Class<?> cls = routerMap.get(path);
+            Intent intent = new Intent(mContext,cls);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intent);
         }
     }
 }
